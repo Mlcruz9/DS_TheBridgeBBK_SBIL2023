497256bcd74bbd43b8d81f6285aafb875ffe3e70
miguellc <mislap87@gmail.com>
2023-09-20 00:28:21 +0200

    W1 D3

---
+++ 1-Ramp_UP/Python/1-Basics/Teoria/prueba.py
@@ -0,0 +1,7 @@
+pizz = input("Buenas tardes, bienvenido al servicio de pedido online ¿Cuántas pizzas desea?: ")
+
+print("Estupendo, se están preparando " + pizz + "pizzas")
+
+direcc = input("Dígame su dirección: ")
+
+print(f'Le mandaremos las {pizz} pizzas a la dirección {direcc}. Muchas gracias por su pedido!')
