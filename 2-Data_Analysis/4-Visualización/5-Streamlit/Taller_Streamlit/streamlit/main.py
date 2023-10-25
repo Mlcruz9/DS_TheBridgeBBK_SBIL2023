93312938ed046257f32efb80e82b8e1c499e245f
miguellc <mislap87@gmail.com>
2023-10-25 09:11:22 +0200

    W6 D3

---
+++ 2-Data_Analysis/4-Visualización/5-Streamlit/Taller_Streamlit/streamlit/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+# from functions import home, datos, cargar_datos, menu_filtros
+#st.beta_expander ahora es expander
+# Este es mi script
+
+st.set_page_config(page_title='Cargatron', layout='wide', page_icon=':battery:')
+
+# st.balloons()
