b54274dc5467fc2ca9ec00a549e0e60979d8786f
miguellc <mislap87@gmail.com>
2023-12-29 21:46:29 +0000

    Nueva Info

--- 4-Data_Engineering/1-APIs/App_Model/ejercicio/app_model_db.py
+++ 4-Data_Engineering/1-APIs/App_Model/ejercicio/app_model_db.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import os
 import pickle
+from xgboost import XGBRegressor
 from sklearn.model_selection import cross_val_score
 import pandas as pd
 import sqlite3
@@ -34,10 +35,60 @@ def predict():
 #2
 @app.route('/v2/ingest_data', methods=['POST'])
 def ingest_data():
-    pass
+
+    tv = request.args.get('tv', None)
+    radio = request.args.get('radio', None)
+    newspaper = request.args.get('newspaper', None)
+    sales = request.args.get('sales')
+
+    connection = sqlite3.connect('data/advertising.db')
+    cursor = connection.cursor()
+
+    query = '''INSERT INTO campañas VALUES (?, ?, ?, ?)'''
+    filters = [tv, radio, newspaper, sales]
+
+    cursor.execute(query, filters)
+
+    result = cursor.execute('''SELECT * FROM campañas''').fetchall()
+    
+    connection.commit()
+    connection.close()
+
+    return result
 
 
 #3
+@app.route('/v2/retrain', methods=['PUT'])
+def retrain():
+    connection = sqlite3.connect('data/advertising.db')
+    cursor = connection.cursor()
 
-# app.run()
+    query = '''SELECT * FROM campañas'''
+    result = cursor.execute(query).fetchall()
+    
+
+    df = pd.DataFrame(data = result, columns = ['tv', 'radio', 'newspaper', 'sales'])
+    df = df.dropna()
+    X = df.iloc[:, :-1]
+    Y = df.iloc[:, -1]
+
+    model = pickle.load(open('data/advertising_model', 'rb'))
+
+    mae_model = abs(cross_val_score(model, X, Y, scoring= 'neg_mean_absolute_error', cv = 5).mean())
+
+    xgb_reg = XGBRegressor()
+    xgb_reg.fit(X, Y)
+
+    mae_new_model = abs(cross_val_score(xgb_reg, X, Y, scoring= 'neg_mean_absolute_error', cv = 5).mean())
+    
+    connection.close()
+
+    if mae_model > mae_new_model:
+        pickle.dump(xgb_reg, open('data/advertising_model', 'wb'))
+        return f'Your model was updated, old mae: {mae_model}, new mae: {mae_new_model}'
+    
+    else:
+        return f'Your old model is better, old mae: {mae_model}, new mae: {mae_new_model}'
+
+app.run()
 
